0c1a4ca9caef8020111bc85b097d65271689f5c5
Stiesel <75696888+Stiesel@users.noreply.github.com>
2025-10-15 21:04:21 +0200

    Implement async setup and unload for sensor
    Add async setup and unload entry functions for sensor.

--- custom_components/aquael_thermometer_link/__init__.py
+++ custom_components/aquael_thermometer_link/__init__.py
@@ -1,1 +1,8 @@
-# required for Home Assistant custom integrations
+async def async_setup_entry(hass, entry):
+    hass.async_create_task(
+        hass.helpers.forward.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
+
+async def async_unload_entry(hass, entry):
+    return await hass.helpers.forward.async_forward_entry_unload(entry, "sensor")
