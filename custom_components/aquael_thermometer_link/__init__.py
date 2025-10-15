b56326c33caef816ee62dd8dcb5b85d113ea5cb9
Stiesel <75696888+Stiesel@users.noreply.github.com>
2025-10-15 21:31:06 +0200

    Refactor async setup and unload entry methods

--- custom_components/aquael_thermometer_link/__init__.py
+++ custom_components/aquael_thermometer_link/__init__.py
@@ -1,8 +1,6 @@
 async def async_setup_entry(hass, entry):
-    hass.async_create_task(
-        hass.helpers.forward.async_forward_entry_setup(entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
     return True
 
 async def async_unload_entry(hass, entry):
-    return await hass.helpers.forward.async_forward_entry_unload(entry, "sensor")
+    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
