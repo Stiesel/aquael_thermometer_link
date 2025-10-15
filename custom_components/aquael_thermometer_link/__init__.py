d5495b62a733f90c6814bae1ab02370273d61da2
Stiesel <75696888+Stiesel@users.noreply.github.com>
2025-10-15 21:36:16 +0200

    Update __init__.py

--- custom_components/aquael_thermometer_link/__init__.py
+++ custom_components/aquael_thermometer_link/__init__.py
@@ -1,6 +1,6 @@
 async def async_setup_entry(hass, entry):
-    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass, entry):
-    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    return await hass.config_entries.async_forward_entry_unloads(entry, ["sensor"])
